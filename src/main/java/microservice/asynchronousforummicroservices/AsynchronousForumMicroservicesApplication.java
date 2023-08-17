fe79daac2f002593d7038829e6d63e169ad77359
Zyuzin-package <zyuzinm4@gmail.com>
2023-08-17 14:41:55 +0400

    init

---
+++ src/main/java/microservice/asynchronousforummicroservices/AsynchronousForumMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package microservice.asynchronousforummicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynchronousForumMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsynchronousForumMicroservicesApplication.class, args);
+	}
+
+}
