218cc2eaf509f4e4fbf1cf5f09ade4aeb47c9bbc
Zyuzin-package <zyuzinm4@gmail.com>
2023-08-18 19:28:13 +0400

    test producer

---
+++ src/main/java/microservice/asynchronousforummicroservices/Listener.java
@@ -0,0 +1,13 @@
+//package microservice.asynchronousforummicroservices;
+//
+//import org.springframework.kafka.annotation.KafkaHandler;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class Listener {
+//    @KafkaListener(topics = "test", groupId = "k")
+//    public void listenGroupFoo(String message) {
+//        System.out.println("Received Message in group foo: " + message);
+//    }
+//}
